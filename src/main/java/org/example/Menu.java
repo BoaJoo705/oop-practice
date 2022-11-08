6dca69173601beb7a30fa1f7d75c86203beee7a7
JooBoa <joocong2265@gmail.com>
2022-11-08 15:21:53 +0900

    part1. MVC 프레임워크 - 음식점에서 음식을 주문하는 과정

---
+++ src/main/java/org/example/Menu.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.List;
+
+public class Menu {
+
+    private final List<MenuItem> menuItems;
+
+    public Menu(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+    //메뉴판 메뉴의 이름을 전달하면 메뉴항목을 반환
+    public MenuItem choose(String name) {
+        return this.menuItems.stream()
+                .filter(menuItem -> menuItem.matches(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 메뉴 이름입니다."));
+//        return new MenuItem("돈까스",5000);
+    }
+}
